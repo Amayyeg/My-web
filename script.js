498eab3e06e2ecb11304ff5cfe265d448a803f05
Amayyeg <anandamay2004@gmail.com>
2025-01-25 22:12:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Welcome to THE AMAXONE! Explore our amazing games.');
+});
+
+document.querySelector('.support-button').addEventListener('click', function() {
+    alert('Support is on the way! How can we assist you?');
+});
